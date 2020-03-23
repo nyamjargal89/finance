53a35bcf6164980e98722944b973925cbaabb68d
Nyamaa <tu.nyamjargal@gmail.com>
2020-03-23 16:52:11 +0800

    delgetsnees utga avdag function

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 //  Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: '.add__description',
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+
+  } 
+
+return {
+  getInput : function(){
+    return {
+      type: document.querySelector(DOMstrings.inputType).value,
+      description: document.querySelector(DOMstrings.inputDescription).value,
+      value: document.querySelector(DOMstrings.inputValue).value
+    };
+  },
+  getDOMstrings: function(){
+    return DOMstrings;
+  }
+};
+})();
+
+
 
 //  Санхүүтэй ажиллах контроллер
 var financeController = (function() {})();
 
 //  Программын холбогч контроллер
 var appController = (function(uController, fnController) {
+    var DOM = uController.getDOMstrings();
     var ctrlAddItem = function() {
         //   1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс мэдээлэл авах хэсэг");
+    console.log(uiController.getInput());
     //   2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     //   3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     //   4. Төсвийг тооцоолно.
     //   5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     }
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  document.querySelector(DOM.addBtn).addEventListener("click", function() {
     ctrlAddItem();
   });
   document.addEventListener("keypress", function(event) {
