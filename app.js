06b33e1925b33ff01d74bf71a20f5a4cf5888fcd
Nyamaa <tu.nyamjargal@gmail.com>
2020-03-23 17:38:54 +0800

    app shineer ehluuleh

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 //  Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {
-
+var userInterfaceController = (function() {
   var DOMstrings = {
     inputType: ".add__type",
-    inputDescription: '.add__description',
+    inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn"
+  };
 
-  } 
-
-return {
-  getInput : function(){
-    return {
-      type: document.querySelector(DOMstrings.inputType).value,
-      description: document.querySelector(DOMstrings.inputDescription).value,
-      value: document.querySelector(DOMstrings.inputValue).value
-    };
-  },
-  getDOMstrings: function(){
-    return DOMstrings;
-  }
-};
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
-
-
 
 //  Санхүүтэй ажиллах контроллер
 var financeController = (function() {})();
 
 //  Программын холбогч контроллер
-var appController = (function(uController, fnController) {
-    var DOM = uController.getDOMstrings();
-    var ctrlAddItem = function() {
-        //   1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+var appController = (function(uiController, fnController) {
+  var ctrlAddItem = function() {
+    //   1. Оруулах өгөгдлийг дэлгэцээс олж авна.
     console.log(uiController.getInput());
     //   2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
     //   3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
     //   4. Төсвийг тооцоолно.
     //   5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-    }
-  document.querySelector(DOM.addBtn).addEventListener("click", function() {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
+  };
+  var setupEventListeners = function() {
+    var DOM = userInterfaceController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+      ctrlAddItem();
+    });
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
+      }
+    });
+
+    
+  };
+  return {
+    init: function() {
+      console.log("Application started ... ");
+      setupEventListeners();
     }
-  });
-})(uiController, financeController);
+  };
+})(userInterfaceController, financeController);
+
+ appController.init();
