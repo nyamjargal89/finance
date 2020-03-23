b2b7e0241254ec515e4ffd484b51e6879f2a6c43
Nyamaa <tu.nyamjargal@gmail.com>
2020-03-23 14:21:07 +0800

    Event listeneruudiig uusgej ugluu

---
+++ app.js
@@ -0,0 +1,25 @@
+//  Дэлгэцтэй ажиллах контроллер
+var uiController = (function() {})();
+
+//  Санхүүтэй ажиллах контроллер
+var financeController = (function() {})();
+
+//  Программын холбогч контроллер
+var appController = (function(uController, fnController) {
+    var ctrlAddItem = function() {
+        //   1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    console.log("Дэлгэцээс мэдээлэл авах хэсэг");
+    //   2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    //   3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+    //   4. Төсвийг тооцоолно.
+    //   5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    }
+  document.querySelector(".add__btn").addEventListener("click", function() {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
