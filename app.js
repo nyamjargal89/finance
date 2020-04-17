53e003ea0183e7667b356c6e25976d406b369b8b
Nyamaa <tu.nyamjargal@gmail.com>
2020-04-17 10:12:46 +0800

    Ura, duuslaa

--- app.js
+++ app.js
@@ -44,6 +44,19 @@ var uiController = (function () {
       document.querySelector(DOMstrings.dateLabel).textContent =
         unuudur.getFullYear() + " оны " + unuudur.getMonth() + " сарын ";
     },
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ", " +
+          DOMstrings.inputDescription +
+          ", " +
+          DOMstrings.inputValue
+      );
+      nodeListForeach(fields, function (el) {
+        el.classList.toggle("red-focus");
+      });
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+    },
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // exp, inc
@@ -308,6 +321,9 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
     document
       .querySelector(DOM.containerDiv)
       .addEventListener("click", function (event) {
